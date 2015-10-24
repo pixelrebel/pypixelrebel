fe37ad975066a33f11d8da94138d31519b4c273e
pixelrebel <github@pixelrebel.com>
2015-10-24 14:04:42 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pixelrebel',
   packages = ['pixelrebel'], 
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'A helper library of common snippets.',
   author = 'Pixel Rebel',
   author_email = 'github@pixelrebel.com',
   url = 'https://github.com/pixelrebel/pypixelrebel',
-  download_url = 'https://github.com/pixelrebel/pypixelrebel/tarball/v0.0.4', 
+  download_url = 'https://github.com/pixelrebel/pypixelrebel/tarball/v0.0.5', 
   keywords = ['helper', 'helpers'],
   classifiers = [],
 )
