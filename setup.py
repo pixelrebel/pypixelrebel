c4b3778af776265a839470e35a7f8a9ef35a85b2
pixelrebel <github@pixelrebel.com>
2015-10-24 12:56:56 -0400

    fix download_url

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'pixelrebel',
-  packages = ['pixelrebel'], # this must be the same as the name above
+  packages = ['pixelrebel'], 
   version = '0.0.1',
   description = 'A helper library of common snippets.',
   author = 'Pixel Rebel',
   author_email = 'github@pixelrebel.com',
-  url = 'https://github.com/pixelrebel/pypixelrebel', # use the URL to the github repo
-  download_url = 'https://github.com/pixelrebel/pypixelrebel/tarball/0.0.1', # I'll explain this in a second
-  keywords = ['helper', 'helpers'], # arbitrary keywords
+  url = 'https://github.com/pixelrebel/pypixelrebel',
+  download_url = 'https://github.com/pixelrebel/pypixelrebel/tarball/v0.0.1', 
+  keywords = ['helper', 'helpers'],
   classifiers = [],
 )
